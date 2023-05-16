294add5f9d8ba2d290325a2c83dd385f91f1cc63
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-05-16 22:41:52 +0300

    Решение ДЗ к 3му  семинару JAVA

---
+++ seminar_3/program_3_2.java
@@ -0,0 +1,24 @@
+package seminar_3;
+
+// Пусть дан произвольный список целых чисел, удалить из него четные числа
+import java.util.ArrayList;
+
+public class program_3_2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>();
+        array = lib.FillRandomArray(10);
+        lib.PrintArray(array);
+        DeleteEvenNumber(array);
+        lib.PrintArray(array);
+    }
+
+    public static void DeleteEvenNumber(ArrayList<Integer> list) {
+        int i = 0;
+        while (i < list.size()) {
+            if (list.get(i) % 2 == 0)
+                list.remove(i);
+            else
+                i++;
+        }
+    }
+}
