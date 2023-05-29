65396b99d8dc5582f77672b577636fe0dbc643dd
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-05-29 21:56:04 +0300

    Решение ДЗ к 6му  семинару JAVA

--- seminar_6/program_6_1.java
+++ seminar_6/program_6_1.java
@@ -1,56 +1,109 @@
 package seminar_6;
 
-import java.util.HashMap;
-import java.util.HashSet;
-
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class program_6_1 {
+    public static void main(String[] args) {
+        Notebook notebook_1 = new Notebook(128, 500, "Windows", "silver");
+        Notebook notebook_2 = new Notebook(256, 1000, "Linux", "silver");
+        Notebook notebook_3 = new Notebook(64, 250, "Windows", "black");
+        Notebook notebook_4 = new Notebook(256, 1000, "Windows", "white");
+        Notebook notebook_5 = new Notebook(512, 3000, "Linux", "black");
+        Notebook notebook_6 = new Notebook(256, 500, "Linux", "black");
+        ArrayList<Notebook> notebooks = new ArrayList<>(
+                Arrays.asList(notebook_1, notebook_2, notebook_3, notebook_4, notebook_5, notebook_6));
+        System.out.println(
+                "Здравствуйте! Для подбора параметров ноутбука введите последовательно критерии вашего выбора!");
+        Map<String, Integer> mapOptions = new HashMap<>();
+        mapOptions.put("amountRam", 0);
+        mapOptions.put("hardDiskCapacity", 0);
+        mapOptions.put("systemOperating", 0);
+        mapOptions.put("colour", 0);
+        Notebook.printInvitationForUser(mapOptions);
+        Scanner scn = new Scanner(System.in);
 
-    public static void main(String[] args) throws Exception {
+        int numberOption = -1;
+        Set<Integer> setNumbersOptions = new HashSet<>();
 
-        Laptop laptop1 = new Laptop("Lenovo IdealPad 5", 8, 256, "Windows 11", "синий", 15.6);
-        Laptop laptop2 = new Laptop("Honor MagicBook 16", 16, 512, "без ОС", "серый", 16.1);
-        Laptop laptop3 = new Laptop("Apple MacBook Air 13", 8, 256, "MacOs", "золотистый", 13.3);
-        Laptop laptop4 = new Laptop("HP 250 G7", 4, 1024, "без ОС", "черный", 15.6);
-        Laptop laptop5 = new Laptop("Xiomi RedmiBook 15", 8, 256, "Windows 11", "серый", 15.6);
-        Laptop laptop6 = laptop1;
+        while (numberOption != 0) {
+            if (setNumbersOptions.size() == 0) {
+                numberOption = scn.nextInt();
+                if (!Notebook.checkNumberOption(numberOption, setNumbersOptions)) {
+                    break;
+                }
+                setNumbersOptions.add(numberOption);
+            }
 
-        Set<Laptop> unicLaptop = new HashSet<Laptop>();
-        unicLaptop.add(laptop1);
-        unicLaptop.add(laptop2);
-        unicLaptop.add(laptop3);
-        unicLaptop.add(laptop4);
-        unicLaptop.add(laptop5);
-        unicLaptop.add(laptop6);
-        System.out.println(laptop1.equals(laptop6));
-        System.out.printf("Всего уникалных ноутбуков: %d \n", unicLaptop.size());
+            if (numberOption == 1) {
+                System.out.println("Введите минимальный объем ОЗУ! ");
+                int minAmountRam = scn.nextInt();
+                mapOptions.put("amountRam", minAmountRam);
+                Notebook.printInvitationForUser(mapOptions);
+                numberOption = scn.nextInt();
 
-        Map<Integer, String> mapCrit = new HashMap<>();
-        mapCrit.put(1, "объем оперативной памяти");
-        mapCrit.put(2, "объем накопителя");
-        mapCrit.put(3, "ОС");
-        mapCrit.put(4, "цвет");
-        mapCrit.put(5, "диагональ");
+                if (!Notebook.checkNumberOption(numberOption, setNumbersOptions)) {
+                    break;
+                }
+                setNumbersOptions.add(numberOption);
+            }
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите желаемые характеристики: 1. объем оперативной памяти: ");
-        int ramUser = sc.nextInt();
-        System.out.println("объем накопителя: ");
-        int storUser = sc.nextInt();
+            if (numberOption == 2) {
+                System.out.println("Введите минимальный объем ЖД! ");
+                int minHardDiskCapacity = scn.nextInt();
+                mapOptions.put("hardDiskCapacity", minHardDiskCapacity);
+                Notebook.printInvitationForUser(mapOptions);
+                numberOption = scn.nextInt();
 
-        System.out.println("диагональ");
-        double digUser = sc.nextDouble();
+                if (!Notebook.checkNumberOption(numberOption, setNumbersOptions)) {
+                    break;
+                }
+                setNumbersOptions.add(numberOption);
+            }
 
-        for (Laptop lap : unicLaptop) {
-            if ((lap.getRam() >= ramUser) && (lap.getStorageCap() >= storUser) && lap.getDiagonal() >= digUser) {
-                System.out.println(lap.toString());
+            if (numberOption == 3) {
+                System.out.println("Для выбора \"Windows\" введите 1,\n для выбора \"Linux\" введите 2:\n");
+                int numberSystemOperating = scn.nextInt();
+                if (numberSystemOperating != 1 && numberSystemOperating != 2) {
+                    System.out.println("Ввели некорректный номер системы!!!");
+                } else {
+                    mapOptions.put("systemOperating", numberSystemOperating);
+                    Notebook.printInvitationForUser(mapOptions);
+                    numberOption = scn.nextInt();
+
+                    if (!Notebook.checkNumberOption(numberOption, setNumbersOptions)) {
+                        break;
+                    }
+                    setNumbersOptions.add(numberOption);
+                }
+            }
+
+            if (numberOption == 4) {
+                System.out.println(
+                        "Для выбора цвета \"серебристый\" введите 1,\n для выбора \"белый\" введите 2:\n, для выбора \"черный\" введите 3:\n");
+                int numberColour = scn.nextInt();
+                if (numberColour < 1 && numberColour > 3) {
+                    System.out.println("Ввели некорректный номер цвета!!!");
+                } else {
+                    mapOptions.put("colour", numberColour);
+                    if (setNumbersOptions.size() == 4) {
+                        break;
+                    }
+                    Notebook.printInvitationForUser(mapOptions);
+                    numberOption = scn.nextInt();
+                    if (!Notebook.checkNumberOption(numberOption, setNumbersOptions)) {
+                        break;
+                    }
+                }
             }
         }
-        sc.close();
 
+        ArrayList<Notebook> setResult = Notebook.getNotebookSetByOptions(notebooks, mapOptions);
+        if (setResult.size() == 0) {
+            System.out.println("К сожалению, по выбранным вами критериям ноутбуков нет!!!");
+        } else {
+            System.out.println("По выбранным вами критериям мы можем предложить следующий список ноутбуков:");
+            System.out.println(setResult);
+        }
+        scn.close();
     }
-
 }
